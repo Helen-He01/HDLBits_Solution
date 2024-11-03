88df2a318dbaea86178a1806d2aafe51ec2289f2
Helen-He01 <110753079+Helen-He01@users.noreply.github.com>
2024-11-03 15:47:39 +0100

    Update 2.4.4 always_if2.v
    Incomplete source code

--- 2 Verilog Language/2.4 Procedures/2.4.4 always_if2.v
+++ 2 Verilog Language/2.4 Procedures/2.4.4 always_if2.v
@@ -5,15 +5,19 @@ module top_module (
     input      arrived,
     input      gas_tank_empty,
     output reg keep_driving  ); //
-
+//需要补充完整output的值
     always @(*) begin
         if (cpu_overheated)
-           shut_off_computer = 1;
+            shut_off_computer = 1; // Shut off the computer if it's overheated.
+        else
+            shut_off_computer = 0; // Otherwise, do not shut off the computer.
     end
-
+//当arrived==1或者gas==0时，keep_arriving = 0
     always @(*) begin
-        if (~arrived)
-           keep_driving = ~gas_tank_empty;
+        if (arrived || gas_tank_empty)
+            keep_driving = 0; // Stop driving if arrived or need to refuel.
+        else
+            keep_driving = 1; // Keep driving if not arrived and gas is not empty.
     end
 
 endmodule
