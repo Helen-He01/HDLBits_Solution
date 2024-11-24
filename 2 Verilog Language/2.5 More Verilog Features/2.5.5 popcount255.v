fd6141b244102f0687cda5e7fc868f369856757d
Shuyan He <110753079+Helen-He01@users.noreply.github.com>
2024-11-24 20:18:14 +0100

    Update 2.5.5 popcount255.v

--- 2 Verilog Language/2.5 More Verilog Features/2.5.5 popcount255.v
+++ 2 Verilog Language/2.5 More Verilog Features/2.5.5 popcount255.v
@@ -1,10 +1,10 @@
 module top_module (
 	input [254:0] in,
-	output reg [7:0] out
+	output reg [7:0] out //don't need to use a reg, but better to use since it can store the data and don't change because of the previous value
 );
 
 	always @(*) begin	// Combinational always block
-		out = 0;
+		out = 0;	//it may store previous value if not use it
 		for (int i=0;i<255;i++)
 			out = out + in[i];
 	end
