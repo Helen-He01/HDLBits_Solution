356f53e70a3b5a995f0c34eb79d12691d4d245f3
Shuyan He <110753079+Helen-He01@users.noreply.github.com>
2024-11-24 17:22:33 +0100

    Update 2.5.1 conditional.v

--- 2 Verilog Language/2.5 More Verilog Features/2.5.1 conditional.v
+++ 2 Verilog Language/2.5 More Verilog Features/2.5.1 conditional.v
@@ -7,3 +7,17 @@ module top_module (
     assign min = (min1<min2)? min1 : min2;
 
 endmodule
+
+//other edition
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+
+    // assign intermediate_result1 = compare? true: false;
+    wire [7:0] a1, a2, a3;
+    assign a1 = a < b ? a:b;
+   	assign a2 = c < a1 ? c:a1;
+    assign a3 = d < a2 ? d:a2;
+    assign min = a3;
+    
+endmodule
