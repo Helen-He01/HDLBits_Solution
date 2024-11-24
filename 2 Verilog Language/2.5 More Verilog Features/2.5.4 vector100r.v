39a1c622d97734e36a33a6cdc9ed04462b6b6ef4
Shuyan He <110753079+Helen-He01@users.noreply.github.com>
2024-11-24 20:07:32 +0100

    Update 2.5.4 vector100r.v

--- 2 Verilog Language/2.5 More Verilog Features/2.5.4 vector100r.v
+++ 2 Verilog Language/2.5 More Verilog Features/2.5.4 vector100r.v
@@ -9,3 +9,18 @@ module top_module(
         end
     end
 endmodule
+
+module top_module ( 
+    input [99:0] in,
+    output [99:0] out
+); 
+    // genvar keyword in Verilog is used to declare a variable specifically for use in a generate block
+    genvar i;
+    // used in repetitive pattern that can be automated
+    generate
+        for (i = 0; i < 100; i = i + 1) begin : reverse_bits
+            assign out[i] = in[99-i];
+        end
+    endgenerate
+    
+endmodule
